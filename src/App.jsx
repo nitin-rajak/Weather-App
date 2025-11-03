70e3ea8f2e184063909c50787b272cd97a3ca2a6
Nitin Rajak <nitinrajak410@gmail.com>
2025-11-03 17:09:13 +0530

    Initial commit - Weather App using React and API

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Weather from './components/weather'
+
+function App() {
+  return (
+    <div className="app">
+      <Weather />
+    </div>
+  )
+}
+
+export default App
